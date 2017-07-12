84e8749b47ca00b4da839ddb79a5b813ed0c1866
Ryan <ryan.edge@creditkarma.com>
2017-07-12 10:35:25 -0400

    fix styles for story list

--- src/stories/story-list.ts
+++ src/stories/story-list.ts
@@ -1,33 +1,4 @@
-import { style, classes } from 'typestyle'
-import { em, px } from 'csx'
-import { flexRoot } from 'csstips'
+import { style } from 'typestyle'
+import { white, em } from 'csx'
 
-export const article = style(flexRoot, {
-    paddingLeft: em(1),
-    $nest: {
-        '&:not(:last-child)': {
-            marginBottom: em(1.5)
-        }
-    }
-})
-
-export const index = style({
-    fontSize: em(1.6),
-    width: px(50)
-})
-
-export const detail = style({
-    marginTop: em(0.5),
-    color: '#666',
-    fontSize: em(0.8)
-})
-
-export const link = style({
-    color: '#666'
-})
-
-export const unstyled = style({
-    textDecoration: 'none'
-})
-
-export const unstyledLink = classes(link, unstyled)
+export default style({ backgroundColor: white.toHexString(), padding: `${em(2)} ${em(2)} 0` })
